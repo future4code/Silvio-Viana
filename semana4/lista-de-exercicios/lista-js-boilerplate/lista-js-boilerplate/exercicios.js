fef94fba9bbb2848c7daf9f1423e8b6958ec2e8a
hsilvioviana <hsilvioviana@hotmail.com>
2021-03-05 14:04:58 -0300

    Exercicio 14 resolvido

--- semana4/lista-de-exercicios/lista-js-boilerplate/lista-js-boilerplate/exercicios.js
+++ semana4/lista-de-exercicios/lista-js-boilerplate/lista-js-boilerplate/exercicios.js
@@ -218,7 +218,13 @@ function imprimeChamada() {
 // Exercício 14
 
 function criaRetangulo(lado1, lado2) {
-   // implemente sua lógica aqui
+   const retangulo = {
+      largura: lado1,
+      altura: lado2,
+      perimetro: 2 * (lado1 + lado2),
+      area: lado1 * lado2
+   }
+   return retangulo
 }
 
 // Exercício 15
