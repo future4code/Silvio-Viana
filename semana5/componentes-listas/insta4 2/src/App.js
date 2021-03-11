779b8e7dd336251f78608b125774d993545b8d43
hsilvioviana <hsilvioviana@hotmail.com>
2021-03-11 15:53:05 -0300

    Resolvido implementacao  1

--- semana5/componentes-listas/insta4 2/src/App.js
+++ semana5/componentes-listas/insta4 2/src/App.js
@@ -3,26 +3,25 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+
   render() {
+
+    const posts = [
+      {nome: 'Paulinha', foto: 'https://picsum.photos/50/50', post: 'https://picsum.photos/200/150'},
+      {nome: 'João', foto: 'https://picsum.photos/50/50?a=1', post: 'https://picsum.photos/200/150?1'},
+      {nome: 'Maria', foto: 'https://picsum.photos/50/50?a=2', post: 'https://picsum.photos/200/150?2'}]
+    
+    const componentes = posts.map((pessoa) => {
+      return <Post key={pessoa.nome}
+        nomeUsuario={pessoa.nome}
+        fotoUsuario={pessoa.foto}
+        fotoPost={pessoa.post}
+      />
+    })
+    
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'Paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-
-        <Post
-          nomeUsuario={'João'}
-          fotoUsuario={'https://picsum.photos/50/50?a=1'}
-          fotoPost={'https://picsum.photos/200/150?1'}
-        />
-
-        <Post
-          nomeUsuario={'Maria'}
-          fotoUsuario={'https://picsum.photos/50/50?a=2'}
-          fotoPost={'https://picsum.photos/200/150?2'}
-        />
+        {componentes}
       </div>
     );
   }
