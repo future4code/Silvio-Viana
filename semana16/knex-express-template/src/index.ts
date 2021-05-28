d968e3f8d6057e0c72aa1bb8753f57067cfe0aed
hsilvioviana <hsilvioviana@hotmail.com>
2021-05-28 20:33:13 -0300

    6-10

--- semana16/knex-express-template/src/index.ts
+++ semana16/knex-express-template/src/index.ts
@@ -15,6 +15,26 @@ app.get("/see/:path", async (req: Request, res: Response) => {
     res.send(result)
 })
 
+//8.
+app.get("/user", async (req: Request, res: Response) => {
+
+    try {
+
+        const query  = req.query.query
+
+        if (!query) { throw new Error("Você deve fornecer uma query com o nickname ou email do usuário que você procura") }
+
+        const result = await connection.raw(`SELECT id, nickname FROM Users 
+        WHERE nickname LIKE "%${query}%" OR email LIKE "%${query}%"`)
+
+        res.status(200).send({ users: result[0]} )
+    }
+    catch (err) {
+
+        res.status(400).send({message: err.message})
+    }
+})
+
 //1.
 app.put("/user", async (req: Request, res: Response) => {
 
@@ -36,6 +56,20 @@ app.put("/user", async (req: Request, res: Response) => {
             if (err.sqlMessage.includes("nickname")) { res.status(400).send("Já existe um usuário com esse nickname") }
             if (err.sqlMessage.includes("email")) { res.status(400).send("Já existe um usuário com esse email") }
         }
+        res.status(400).send(err.message)
+    }
+})
+
+//6.
+app.get("/user/all", async (req: Request, res: Response) => {
+
+    try {
+
+        const result = await connection.raw("SELECT id, name FROM Users")
+        res.status(400).send({users: result[0]})
+    }
+    catch (err) {
+
         res.status(400).send(err.message)
     }
 })
@@ -86,30 +120,31 @@ app.post("/user/edit/:id", async (req: Request, res: Response) => {
     }
 })
 
-//4.
-app.put("/task", async (req: Request, res: Response) => {
+//7.
+app.get("/task", async (req: Request, res: Response) => {
 
     try {
 
-        const title: string = req.body.title
-        const description: string = req.body.description
-        const limitDate: string = req.body.limitDate
-        const creatorUserId: number = Number(req.body.creatorUserId)
+        const creatorUserId: number = Number(req.query.creatorUserId)
 
-        if (isNaN(creatorUserId)) { throw new Error ("O creatorUserId deve ser um número")}
-        if (!title || !description || !limitDate || !creatorUserId) 
-        { throw new Error("Você deve informar: title, description, limitDate e creatorUserId") }
-        if (!formatoValidoData(limitDate)) { throw new Error("Formato inválido de data")}
+        if (isNaN(creatorUserId)) { throw new Error("O creatorUserId deve ser um número") }
+        if (!creatorUserId) { throw new Error("Você deve fornecer: creatorUserId") }
 
-        const result = await connection.raw(`INSERT INTO Tasks (title, description, limitDate, creatorUserId)
-        VALUES ("${title}", "${description}", "${formatarStringToData(limitDate)}", ${creatorUserId})`)
+        const result = await connection.raw(`SELECT t.id as taskId, t.title, t.description, t.limitDate, t.status,
+        u.id as creatorUserId, u.nickname as creatorUserNickname
+        FROM Tasks t JOIN Users u ON u.id = t.creatorUserId
+        WHERE u.id = ${creatorUserId}`)
 
-        res.status(200).send("Tarefa criada com sucesso")
+        for (let i = 0; i < result[0].length; i++) {
+
+            result[0][i].limitDate = formatarDataToString(result[0][i].limitDate)
+        }
+
+        res.status(200).send({tasks: result[0]})
     }
     catch(err) {
 
-        if (err.sqlMessage.inclues("too long")) { res.status(400).send("Algum dos campos tem mais caracteres que 255")}
-        res.status(400).send(err.sqlMessage)
+        res.status(400).send({ message: err.message })
     }
 })
 
@@ -137,3 +172,78 @@ app.get("/task/:id", async (req: Request, res: Response) => {
         res.status(200).send(err.message)
     }
 })
+
+//10.
+app.get("/task/:id/responsible", async (req: Request, res: Response) => {
+
+    try {
+
+        const id: number = Number(req.params.id)
+        if (!id || isNaN(id)) { throw new Error("Você deve fornecer um id e ele deve ser um número") }
+
+        const result = await connection.raw(`SELECT u.id, u.nickname FROM Users u JOIN UserTaskRelation ut
+        ON u.id = ut.user_id WHERE ut.task_id = ${id}`)
+        
+        if (result[0].length === 0) { throw new Error("Responsáveis não encontrados") }
+        res.status(200).send({users: result[0]})
+    }
+    catch(err) {
+
+        res.status(400).send({ message: err.message })
+    }
+})
+
+//9.
+app.post("/task/responsible", async (req: Request, res: Response) => {
+
+    try {
+
+        const task_id: number = Number(req.body.task_id)
+        const responsible_user_id: number = Number(req.body.responsible_user_id)
+
+        if (!task_id || !responsible_user_id) { 
+            throw new Error("Você deve fornecer: task_id e responsible_user_id e ambos devem ser número") }
+
+        await connection.raw(`INSERT INTO UserTaskRelation VAlUES (${responsible_user_id}, ${task_id})`)
+
+        res.status(200).send({message: "Tarefa atribuída com sucesso"})
+    }
+    catch(err) {
+
+        if (err.errno === 1452) {
+
+            if (err.sqlMessage.includes("user_id")) { res.status(400).send({message: "Id de usuário não encontrado"}) }
+            if (err.sqlMessage.includes("task_id")) { res.status(400).send({message: "Id de tarefa não encontrado"}) }
+        }
+
+        res.status(400).send({ message: err })
+    }
+})
+
+//4.
+app.put("/task", async (req: Request, res: Response) => {
+
+    try {
+
+        const title: string = req.body.title
+        const description: string = req.body.description
+        const limitDate: string = req.body.limitDate
+        const creatorUserId: number = Number(req.body.creatorUserId)
+
+        if (isNaN(creatorUserId)) { throw new Error ("O creatorUserId deve ser um número")}
+        if (!title || !description || !limitDate || !creatorUserId) 
+        { throw new Error("Você deve informar: title, description, limitDate e creatorUserId") }
+        if (!formatoValidoData(limitDate)) { throw new Error("Formato inválido de data")}
+
+        const result = await connection.raw(`INSERT INTO Tasks (title, description, limitDate, creatorUserId)
+        VALUES ("${title}", "${description}", "${formatarStringToData(limitDate)}", ${creatorUserId})`)
+
+        res.status(200).send("Tarefa criada com sucesso")
+    }
+    catch(err) {
+
+        if (err.sqlMessage.inclues("too long")) { res.status(400).send("Algum dos campos tem mais caracteres que 255")}
+        res.status(400).send(err.sqlMessage)
+    }
+})
+
