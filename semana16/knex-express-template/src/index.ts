54ba037fd81b9e091690c5d1fe106f598ad228c4
hsilvioviana <hsilvioviana@hotmail.com>
2021-05-29 15:04:20 -0300

    16-20

--- semana16/knex-express-template/src/index.ts
+++ semana16/knex-express-template/src/index.ts
@@ -121,13 +121,45 @@ app.post("/user/edit/:id", async (req: Request, res: Response) => {
     }
 })
 
-//7. e 13.
+//20.
+app.delete("/user/:id", async (req: Request, res: Response) => {
+
+    try {
+
+        const id: number = Number(req.params.id)
+
+        if (!id || isNaN(id)) { throw new Error("Você deve fornecer o id do usuário e ele deve ser um número") }
+
+        const user = await connection.raw(`SELECT * FROM Users WHERE id = ${id}`)
+        if (user[0].length === 0) { throw new Error("Usuário não encontrado") }
+
+        const tasks = await connection.raw(`SELECT id FROM Tasks WHERE creatorUserId = ${id}`)
+
+        for (let task of tasks[0]) {
+
+            await connection.raw(`DELETE FROM UserTaskRelation WHERE task_id = ${task.id} OR user_id = ${id}`)
+            await connection.raw(`DELETE FROM Tasks WHERE id = ${task.id}`)
+        }
+
+        await connection.raw(`DELETE FROM Users WHERE id = ${id}`)
+
+        res.status(200).send({ message: "Usuário apagado com sucesso" })
+
+    }
+    catch(err) {
+
+        res.status(400).send({ message: err.message })
+    }
+})
+
+//7. , 13. e 17.
 app.get("/task", async (req: Request, res: Response) => {
 
     try {
 
         const creatorUserId: number = Number(req.query.creatorUserId)
         const status = req.query.status
+        const termo = req.query.termo
         let result = []
 
         if (creatorUserId) {
@@ -146,7 +178,14 @@ app.get("/task", async (req: Request, res: Response) => {
             FROM Tasks t JOIN Users u ON u.id = t.creatorUserId
             WHERE t.status LIKE "%${status}%"`)
         }
-        else { throw new Error("Você deve fornecer: creatorUserId ou status na query") }
+        else if (termo) {
+
+            result = await connection.raw(`SELECT t.id as taskId, t.title, t.description, t.limitDate, t.status,
+            u.id as creatorUserId, u.nickname as creatorUserNickname
+            FROM Tasks t JOIN Users u ON u.id = t.creatorUserId
+            WHERE t.title LIKE "%${termo}%" OR t.description LIKE "%${termo}%"`)
+        }
+        else { throw new Error("Você deve fornecer: creatorUserId, status ou termo pela chave query na url") }
         
         for (let i = 0; i < result[0].length; i++) {
 
@@ -235,30 +274,39 @@ app.get("/task/:id/responsible", async (req: Request, res: Response) => {
     }
 })
 
-//9.
+//9. e 16.
 app.post("/task/responsible", async (req: Request, res: Response) => {
 
     try {
 
         const task_id: number = Number(req.body.task_id)
-        const responsible_user_id: number = Number(req.body.responsible_user_id)
+        const ids: number[] = req.body.responsible_user_ids
 
-        if (!task_id || !responsible_user_id) { 
-            throw new Error("Você deve fornecer: task_id e responsible_user_id e ambos devem ser número") }
+        if (!task_id || !ids)
+        { throw new Error("Você deve fornecer: task_id (número) e responsible_user_ids (array de números)") }
 
-        await connection.raw(`INSERT INTO UserTaskRelation VAlUES (${responsible_user_id}, ${task_id})`)
+        if (!(typeof ids !== "string" && ids.length > 0 && !ids.some(isNaN))) 
+        { throw new Error("responsible_user_ids deve ser um array de números") }
 
-        res.status(200).send({message: "Tarefa atribuída com sucesso"})
+        const task = await connection.raw(`SELECT * FROM Tasks WHERE id = ${task_id}`)
+        if (task[0].length === 0) { throw new Error("Tarefa não encontrada") }
+
+        for (let i = 0; i < ids.length; i++) {
+
+            const user = await connection.raw(`SELECT * FROM Users WHERE id = ${Number(ids[i])}`)
+            if (user[0].length === 0) { throw new Error(`O id de usuário no índice ${i} não foi encontrado`) }
+        }
+
+        for (let id of ids) {
+
+            await connection.raw(`INSERT INTO UserTaskRelation VAlUES (${id}, ${task_id})`)
+        }
+
+        res.status(200).send({ message: "Tarefa atribuída com sucesso" })
     }
     catch(err) {
 
-        if (err.errno === 1452) {
-
-            if (err.sqlMessage.includes("user_id")) { res.status(400).send({message: "Id de usuário não encontrado"}) }
-            if (err.sqlMessage.includes("task_id")) { res.status(400).send({message: "Id de tarefa não encontrado"}) }
-        }
-
-        res.status(400).send({ message: err })
+        res.status(400).send({ message: err.message })
     }
 })
 
@@ -277,35 +325,68 @@ app.put("/task", async (req: Request, res: Response) => {
         { throw new Error("Você deve informar: title, description, limitDate e creatorUserId") }
         if (!formatoValidoData(limitDate)) { throw new Error("Formato inválido de data")}
 
-        const result = await connection.raw(`INSERT INTO Tasks (title, description, limitDate, creatorUserId)
+        await connection.raw(`INSERT INTO Tasks (title, description, limitDate, creatorUserId)
         VALUES ("${title}", "${description}", "${formatarStringToData(limitDate)}", ${creatorUserId})`)
 
         res.status(200).send("Tarefa criada com sucesso")
     }
     catch(err) {
 
-        if (err.sqlMessage.inclues("too long")) { res.status(400).send("Algum dos campos tem mais caracteres que 255")}
-        res.status(400).send(err.sqlMessage)
+        if (err.sqlMessage.includes("too long")) { res.status(400).send("Algum dos campos tem mais caracteres que 255")}
+
+        res.status(400).send({ message: err.message })
     }
 })
 
-//12.
+//12. e 18.
 app.post("/task/status/edit/", async (req: Request, res: Response) => {
 
     try {
 
         const status: string = req.body.status
-        const taskId: number = Number(req.body.taskId)
+        const ids: number[] = req.body.task_ids
 
-        if (!status || !taskId) { throw new Error("Você deve fornecer: status e taskId") }
-        if (isNaN(taskId)) { throw new Error ("O creatorUserId deve ser um número") }
+        if (!status || !ids) { throw new Error("Você deve fornecer: status e taskId") }
 
-        const result = await connection.raw(` UPDATE Tasks SET status = "${status}" WHERE id = ${taskId}`)
+        if (!(typeof ids !== "string" && ids.length > 0 && !ids.some(isNaN))) 
+        { throw new Error("task_ids deve ser um array de números") }
 
-        if (result[0].affectedRows === 0) { throw new Error("Tarefa não encontrada") }
-        if (result[0].changedRows === 0) { throw new Error("A tarefa já possui esse status") }
+        for (let i = 0; i < ids.length; i++) {
 
-        res.status(200).send({message: "Status da tarefa atualizado com sucesso"})
+            const task = await connection.raw(`SELECT * FROM Tasks WHERE id = ${Number(ids[i])}`)
+            if (task[0].length === 0) { throw new Error(`O id de tarefa no índice ${i} não foi encontrado`) }
+        }
+
+        for (let id of ids) {
+
+            await connection.raw(` UPDATE Tasks SET status = "${status}" WHERE id = ${id}`)
+        }
+
+        res.status(200).send({message: "Atualização feita com sucesso"})
+    }
+    catch(err) {
+
+        res.status(400).send({ message: err.message })
+    }
+})
+
+//19.
+app.delete("/task/:id", async (req: Request, res: Response) => {
+
+    try {
+
+        const id: number = Number(req.params.id)
+
+        if (!id || isNaN(id)) { throw new Error("Você deve fornecer o id da tarefa e ela deve ser um número") }
+
+        const task = await connection.raw(`SELECT * FROM Tasks WHERE id = ${id}`)
+        if (task[0].length === 0) { throw new Error("Tarefa não encontrada") }
+
+        await connection.raw(`DELETE FROM UserTaskRelation WHERE task_id = ${id}`)
+        await connection.raw(`DELETE FROM Tasks WHERE id = ${id}`)
+
+        res.status(200).send({ message: "Tarefa apagada com sucesso" })
+
     }
     catch(err) {
 
