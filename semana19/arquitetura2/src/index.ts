ebad16702508f651693d22b5a9ce8c2db6a83ea6
hsilvioviana <hsilvioviana@hotmail.com>
2021-06-15 16:17:54 -0300

    Exercicio 1

--- semana19/arquitetura2/src/index.ts
+++ semana19/arquitetura2/src/index.ts
@@ -1,12 +1,8 @@
 import { app } from "./controller/app"
-import { signup } from './controller/user/signup'
-import { login } from './controller/user/login'
-import { createTask } from './controller/task/createTask'
-import { getTaskById } from './controller/task/getTaskById'
+import { taskRouter } from "./routes/taskRouter"
+import { userRouter } from "./routes/userRouter"
 
-app.post('/user/signup', signup)
-app.post('/user/login', login)
 
-app.put('/task', createTask)
-app.get('/task/:id', getTaskById)
+app.use("/user", userRouter)
+app.use("/task", taskRouter)
 
