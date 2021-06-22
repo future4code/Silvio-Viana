c8026b285fc348b2957891dc3cdcf0956e793b24
hsilvioviana <hsilvioviana@hotmail.com>
2021-06-22 06:58:14 -0300

    Exercicio 7

--- semana20/jest/tests/createPost.test.ts
+++ semana20/jest/tests/createPost.test.ts
@@ -3,20 +3,33 @@ import { Post, createPost, getPostById, deletePostById } from "../src/handlePost
 describe("Testando criação de posts", () => {
 
     const post1: Post = {id: "xlr8", title: "post1", description: "blabla"}
+    const post2: Post = {id: "xlr9", title: "post2", description: "blablabla"}
 
     test("Criando post1", async () => {
 
-        const post: Post = {id: "xlr8", title: "post1", description: "blabla"}
+        await createPost(post1)
 
-        await createPost(post)
+        const result = await getPostById(post1)
 
-        const result = await getPostById(post)
+        expect(result).toEqual(post1)
+    })
 
-        expect(result).toEqual({id: "xlr8", title: "post1", description: "blabla"})
+    test("Criando post2 duas vezes", async () => {
+
+        try {
+
+            await createPost(post2)
+            await createPost(post2)
+        }
+        catch(error) {
+
+            expect(error).not.toBe(undefined)
+        }
     })
 
     afterAll (async () => {
 
         await deletePostById(post1)
+        await deletePostById(post2)
     })
 })
